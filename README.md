8393bd8eee260ff8d9b58ecf98cda9380a673e70
Archer Heffern <heffernarcher@gmail.com>
2024-04-23 01:01:48 -0400

    organization and tests

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 - [Maelstrom](https://github.com/jepsen-io/maelstrom)
 
 "A series of distributed systems challenges brought to you by fly.io"
+
+# Set Up
+1. Install go
+2. Install Maelstrom into the parent directory
+
+# Running
+```bash
+./run.sh
+```
+
+# Testing
+```bash
+cat ./tests/1 | ./challenges/node
+```
--- challenges/node.go
+++ challenges/node.go
@@ -1,30 +1,46 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
+	"encoding/json"
+	"log"
 
-    maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	n := maelstrom.NewNode()
+	handle_echo(n)
+	handle_generate_id(n)
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handle_echo(n *maelstrom.Node) {
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-
-		// Update the message type to return back.
 		body["type"] = "echo_ok"
-
-		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
 	})
+}
 
-	if err := n.Run(); err != nil {
-		log.Fatal(err)
-	}
+var i int32 = 0
 
+func handle_generate_id(n *maelstrom.Node) {
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		body["type"] = "generate_ok"
+		body["id"] = i
+		i += 1
+		return n.Reply(msg, body)
+	})
 }
