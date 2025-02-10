290d4928f473a438a458f270d24d063d8ac2f383
ArcherHeffern <heffernarcher@gmail.com>
2025-02-10 14:33:44 -0500

    challenge 3d complete

--- challenges/node.go
+++ challenges/node.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"slices"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -13,24 +15,30 @@ import (
 
 func main() {
 	n := maelstrom.NewNode()
-	// 1
+	// === 1 ===
 	// handle_echo(n)
-	// 2
+
+	// === 2 ===
 	// handle_generate_id(n)
-	// 3a
+
+	// === 3a ===
 	// handle_read_a(n)
 	// handle_broadcast_a(n)
 	// handle_topology_a(n)
-	// 3a
-	handle_read_a(n)
-	handle_broadcast_a(n)
-	handle_topology_a(n)
-	// 3b-c
-	handle_read_b(n)
-	handle_broadcast_b(n)
-	handle_topology_b(n)
-	// 4
-	// 5
+
+	// === 3b-c ===
+	// handle_read_b(n)
+	// handle_broadcast_b(n)
+	// handle_topology_b(n)
+
+	// === 3d ===
+	handle_read_d(n)
+	handle_broadcast_d(n)
+	handle_topology_d(n)
+
+	// === 4 ===
+
+	// === 5 ===
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
@@ -124,8 +132,6 @@ func handle_topology_a(n *maelstrom.Node) {
 // ############
 // Challenge 3b-c: Multi Node Broadcast Service
 // ############
-const FANOUT = 3
-
 func handle_read_b(n *maelstrom.Node) {
 	n.Handle("read", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
@@ -171,4 +177,88 @@ func handle_topology_b(n *maelstrom.Node) {
 		res_body["type"] = "topology_ok"
 		return n.Reply(msg, res_body)
 	})
+}
+
+// ############
+// Challenge 3d: Single Node Broadcast Service
+// ############
+// I tried to implement a gossip protocol here, but 2-4 messages were usually were dropped. In addition, this was not efficient enough to be accepted.
+var seen_map = make(map[float64]bool, 0)
+var m sync.Mutex
+
+func handle_read_d(n *maelstrom.Node) {
+	n.Handle("read", func(msg maelstrom.Message) error {
+		m.Lock()
+		var seen_list []float64
+		for k, _ := range seen_map {
+			seen_list = append(seen_list, k)
+		}
+		m.Unlock()
+		return n.Reply(msg, map[string]any{
+			"type":     "read_ok",
+			"messages": seen_list,
+		})
+	})
+}
+
+func handle_broadcast_d(n *maelstrom.Node) {
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var req_body map[string]any
+		if err := json.Unmarshal(msg.Body, &req_body); err != nil {
+			return err
+		}
+		go func() {
+			n.Reply(msg, map[string]any{
+				"type": "broadcast_ok",
+			})
+		}()
+
+		var message = req_body["message"].(float64)
+		m.Lock()
+		seen_map[message] = true
+		m.Unlock()
+		if n.ID() == n.NodeIDs()[0] {
+			for _, dest := range n.NodeIDs() {
+				if dest == n.ID() || dest == msg.Src {
+					continue
+				}
+				go func() {
+					n.RPC(dest, req_body, nil)
+				}()
+			}
+		} else if msg.Src != n.NodeIDs()[0] { // Make sure we don't create an infinite sending loop
+			go func() {
+				n.RPC(n.NodeIDs()[0], req_body, nil)
+			}()
+		}
+
+		return nil
+	})
+}
+
+func handle_topology_d(n *maelstrom.Node) {
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		return n.Reply(msg, map[string]any{
+			"type": "topology_ok",
+		})
+	})
+}
+
+// ############
+// UTILS
+// ############
+func select_n_random(list []string, n int) []string {
+	if n > len(list) {
+		// If n exceeds the list size, return the entire list (or handle error).
+		n = len(list)
+	}
+
+	// Shuffle the copied list in place.
+	rand.Shuffle(len(list), func(i, j int) {
+		list[i], list[j] = list[j], list[i]
+	})
+
+	// Return the first n elements from the shuffled copy.
+	return list[:n]
 }
